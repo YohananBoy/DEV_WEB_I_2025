1cf756dcc6456fa26a01f99b4c494476147017ae
Yohanan Boy <14911025786@cefet-rj.br>
2025-09-11 16:05:29 -0300

    json e fetch

--- Aulas/Aula 15/carregaTabelaCliente.js
+++ Aulas/Aula 15/carregaTabelaCliente.js
@@ -1,24 +1,20 @@
-let clientes = {
-    lista: [
-        {nome: "Jose", id: 2, nascimento: "Bom Jardim"},
-        {nome: "Miguel", id: 3, nascimento: "Nova Friburgo"},
-        {nome: "Hanan", id: 4, nascimento: "Cachoeiras de Macacu"},
-    ]
-}
-
 document.addEventListener("DOMContentLoaded", (ev) => {
-    ev.preventDefault()
-    let tabela = document.getElementsByTagName("tbody")[0]
-    clientes.lista.forEach(cliente => {
-        let linha = document.createElement("tr")
-        tabela.appendChild(linha)
+  ev.preventDefault()
+  let tabela = document.getElementsByTagName("tbody")[0]
+  fetch("http://localhost:3000/clientes").then(async (response) => {
+    let clientes = await response.json()
+    console.log(clientes)
+    clientes.forEach((cliente) => {
+      let linha = document.createElement("tr")
+      tabela.appendChild(linha)
 
-        let colunaNome = document.createElement("td")
-        colunaNome.textContent = cliente.nome
-        tabela.appendChild(colunaNome)
-        
-        let colunaNascimento = document.createElement("td")
-        colunaNascimento.textContent = cliente.nascimento
-        tabela.appendChild(colunaNascimento)
+      let colunaNome = document.createElement("td")
+      colunaNome.textContent = cliente.nome
+      tabela.appendChild(colunaNome)
+
+      let colunaNascimento = document.createElement("td")
+      colunaNascimento.textContent = cliente.nascimento
+      tabela.appendChild(colunaNascimento)
     })
+  })
 })
--- Aulas/Aula 15/carregaTabelaProduto.js
+++ Aulas/Aula 15/carregaTabelaProduto.js
@@ -1,31 +1,29 @@
-let produtos = [
-    { id: 1, nome: "Arroz", categoria: "Alimentos", preco: 25.90, estoque: true },
-    { id: 2, nome: "Feijão", categoria: "Alimentos", preco: 9.50, estoque: true },
-    { id: 3, nome: "Detergente", categoria: "Limpeza", preco: 2.50, estoque: false },
-    { id: 4, nome: "Shampoo", categoria: "Higiene", preco: 12.90, estoque: true }
-  ];
-
 document.addEventListener("DOMContentLoaded", (ev) => {
     ev.preventDefault()
     let tabela = document.getElementsByTagName("tbody")[0]
-    produtos.forEach(produto => {
-        let linha = document.createElement("tr")
-        tabela.appendChild(linha)
-
-        let colunaNome = document.createElement("td")
-        colunaNome.textContent = produto.nome
-        tabela.appendChild(colunaNome)
+    fetch("http://localhost:3000/produtos").then(
+        async response => {
+            let produtos = await response.json()
+            produtos.forEach(produto => {
+                let linha = document.createElement("tr")
+                tabela.appendChild(linha)
         
-        let colunaCategoria = document.createElement("td")
-        colunaCategoria.textContent = produto.categoria
-        tabela.appendChild(colunaCategoria)
-
-        let colunaPreco = document.createElement("td")
-        colunaPreco.textContent = produto.preco
-        tabela.appendChild(colunaPreco)
+                let colunaNome = document.createElement("td")
+                colunaNome.textContent = produto.nome
+                tabela.appendChild(colunaNome)
+                
+                let colunaCategoria = document.createElement("td")
+                colunaCategoria.textContent = produto.categoria
+                tabela.appendChild(colunaCategoria)
         
-        let colunaEstoque = document.createElement("td")
-        colunaEstoque.textContent = produto.estoque ? "Sim" : "Não"
-        tabela.appendChild(colunaEstoque)
-    })
+                let colunaPreco = document.createElement("td")
+                colunaPreco.textContent = produto.preco
+                tabela.appendChild(colunaPreco)
+                
+                let colunaEstoque = document.createElement("td")
+                colunaEstoque.textContent = produto.estoque ? "Sim" : "Não"
+                tabela.appendChild(colunaEstoque)
+            })
+        }
+    )
 })
