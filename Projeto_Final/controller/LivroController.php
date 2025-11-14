8465775e8cc5c0808f600aefcc838b2ae7ce9d88
Yohanan <14911025786@cefet-rj.br>
2025-11-14 11:11:11 -0300

    Registrar Retirada

--- Projeto_Final/controller/LivroController.php
+++ Projeto_Final/controller/LivroController.php
@@ -18,7 +18,8 @@ class LivroController implements GenericController
             $dadosRecebidos->anoPublicacao,
             $dadosRecebidos->genero,
             $dadosRecebidos->localizacao,
-            $dadosRecebidos->ISSN
+            $dadosRecebidos->ISSN,
+            $dadosRecebidos->disponivel
         );
         $livro->cadastrar($this->conn);
     }
@@ -29,15 +30,19 @@ class LivroController implements GenericController
     }
     public function alterar($dadosRecebidos)
     {
-        $livro                = Livro::pegaPorId($dadosRecebidos->id, $this->conn);
-        $livro->titulo        = $dadosRecebidos["titulo"];
-        $livro->autor         = $dadosRecebidos["autor"];
-        $livro->editora       = $dadosRecebidos["editora"];
-        $livro->anoPublicacao = $dadosRecebidos["anoPublicacao"];
-        $livro->genero        = $dadosRecebidos["genero"];
-        $livro->localizacao   = $dadosRecebidos["localizacao"];
-        $livro->ISSN          = $dadosRecebidos["ISSN"];
+        $livro = Livro::pegaPorId($dadosRecebidos->id, $this->conn);
+
+        $livro->titulo        = $dadosRecebidos->titulo;
+        $livro->autor         = $dadosRecebidos->autor;
+        $livro->editora       = $dadosRecebidos->editora;
+        $livro->anoPublicacao = $dadosRecebidos->anoPublicacao;
+        $livro->genero        = $dadosRecebidos->genero;
+        $livro->localizacao   = $dadosRecebidos->localizacao;
+        $livro->ISSN          = $dadosRecebidos->ISSN;
+        $livro->disponivel    = $dadosRecebidos->disponivel;
+
         $livro->alterar($this->conn);
+
     }
     public function remover($dadosRecebidos)
     {
--- Projeto_Final/model/livro.class.php
+++ Projeto_Final/model/livro.class.php
@@ -10,6 +10,7 @@ class Livro extends ClassePai
     public $genero;
     public $localizacao;
     public $ISSN;
+    public $disponivel;
 
 //     CREATE TABLElivro(
 //     id BIGINTPRIMARYKEYNOTNULLAUTO_INCREMENT,
@@ -19,8 +20,21 @@ class Livro extends ClassePai
 //     anoPublicacao INT,
 //     genero VARCHAR(30),
 //     localizacao CHAR(2),
-//     ISSN VARCHAR(9)
+//     ISSN VARCHAR(9),
+//     disponivel BOOLEAN
 // )
+
+// INSERT INTOlivro(titulo, autor, editora, anoPublicacao, genero, localizacao, ISSN, disponivel)VALUES
+// ('O Senhor dos Anéis', 'J. R. R. Tolkien', 'HarperCollins', 1954, 'Fantasia', 'A1', '1234-6789', 1),
+// ('Dom Casmurro', 'Machado de Assis', 'Editora Globo', 1899, 'Romance', 'B2', '9876-4321', 1),
+// ('1984', 'George Orwell', 'Companhia das Letras', 1949, 'Distopia', 'C3', '1112-2333', 1),
+// ('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'HarperCollins', 1943, 'Infantil', 'D4', '4445-5666', 1),
+// ('Harry Potter e a Pedra Filosofal', 'J. K. Rowling', 'Rocco', 1997, 'Fantasia', 'E5', '7778-8999', 1),
+// ('A Revolução dos Bichos', 'George Orwell', 'Companhia das Letras', 1945, 'Sátira', 'F6', '1011-2131', 1),
+// ('O Hobbit', 'J. R. R. Tolkien', 'HarperCollins', 1937, 'Fantasia', 'G7', '1415-6171', 1),
+// ('A Metamorfose', 'Franz Kafka', 'Editora 34', 1915, 'Ficção', 'H8', '1819-0212', 1),
+// ('Orgulho e Preconceito', 'Jane Austen', 'Penguin', 1813, 'Romance', 'I9', '2223-4252', 1),
+//     ('It: A Coisa', 'Stephen King', 'Suma', 1986, 'Terror', 'J1', '2627-8293', 1);
 
     public function toEntity($dados)
     {
@@ -32,20 +46,22 @@ class Livro extends ClassePai
             $dados[4],
             $dados[5],
             $dados[6],
-            $dados[7]
+            $dados[7],
+            $dados[8]
         );
     }
 
     public function cadastrar($conn)
     {
-        $SQL = "INSERT INTO livro (titulo, autor, editora, anoPublicacao, genero, localizacao, ISSN) VALUES (
+        $SQL = "INSERT INTO livro (titulo, autor, editora, anoPublicacao, genero, localizacao, ISSN, disponivel) VALUES (
             '$this->titulo',
             '$this->autor',
             '$this->editora',
             '$this->anoPublicacao',
             '$this->genero',
             '$this->localizacao',
-            '$this->ISSN'
+            '$this->ISSN',
+            '$this->disponivel'
         )";
         $resultado = $conn->query($SQL);
         if ($resultado) {
@@ -62,16 +78,17 @@ class Livro extends ClassePai
             anoPublicacao = '$this->anoPublicacao',
             genero = '$this->genero',
             localizacao = '$this->localizacao',
-            ISSN = '$this->ISSN'
+            ISSN = '$this->ISSN',
+            disponivel = '$this->disponivel'
         WHERE id = $this->id";
         $conn->query($SQL);
     }
     public static function pegaPorId($id, $conn)
     {
         $SQL       = "SELECT * FROM livro WHERE id = $id";
-        $resultado = $conn->mysql_query($SQL);
+        $resultado = $conn->query($SQL);
         if ($resultado) {
-            $dados = $conn->fetch_array($resultado);
+            $dados = $resultado->fetch_array();
             return new Livro(
                 $dados['id'],
                 $dados['titulo'],
@@ -80,12 +97,13 @@ class Livro extends ClassePai
                 $dados['anoPublicacao'],
                 $dados['genero'],
                 $dados['localizacao'],
-                $dados['ISSN']
+                $dados['ISSN'],
+                $dados['disponivel'],
             );
         }
     }
 
-    public function __construct($id, $titulo, $autor, $editora, $anoPublicacao, $genero, $localizacao, $ISSN)
+    public function __construct($id, $titulo, $autor, $editora, $anoPublicacao, $genero, $localizacao, $ISSN, $disponivel)
     {
         parent::__construct($id, "database/livro.txt");
         $this->titulo        = $titulo;
@@ -95,6 +113,7 @@ class Livro extends ClassePai
         $this->genero        = $genero;
         $this->localizacao   = $localizacao;
         $this->ISSN          = $ISSN;
+        $this->disponivel    = $disponivel;
     }
 
     public static function listar($filtroNome, $conn)
@@ -111,7 +130,8 @@ class Livro extends ClassePai
                 $dados['anoPublicacao'],
                 $dados['genero'],
                 $dados['localizacao'],
-                $dados['ISSN']
+                $dados['ISSN'],
+                $dados['disponivel'],
             );
             array_push($retorno, $livro);
         }
@@ -127,6 +147,7 @@ class Livro extends ClassePai
         $this->anoPublicacao . self::SEPARADOR .
         $this->genero . self::SEPARADOR .
         $this->localizacao . self::SEPARADOR .
-        $this->ISSN;
+        $this->ISSN . self::SEPARADOR .
+        $this->disponivel;
     }
 }
